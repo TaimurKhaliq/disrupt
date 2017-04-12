4e2a892a26c7e67051543190208e9521cd5fd622
Taimur Khaliq <taimur.khaliq@mobilelive.ca>
2017-04-12 19:30:46 -0400

    basic webpack config

---
+++ src/app.js
@@ -0,0 +1,13 @@
+var path = require('path');
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    }
+};
